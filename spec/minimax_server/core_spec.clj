ece2d369e569a1d5c5ac14980bb246e36bdc0925
nystromb <bnystrom@siu.edu>
2016-01-13 15:08:55 -0600

    make sure we can use server jar files

--- spec/minimax_server/core_spec.clj
+++ spec/minimax_server/core_spec.clj
@@ -1,7 +1,8 @@
 (ns minimax-server.core-spec
+  (:import )
   (:require [speclj.core :refer :all]
             [minimax-server.core :refer :all]))
 
-(describe "a test"
-  (it "passes"
-    (should true)))
+(describe "temporary tests"
+  (it "can use things from http server jar"
+    (should-be-a scarvill.httpserver.response.ResponseBuilder (new scarvill.httpserver.response.ResponseBuilder))))
