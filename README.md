9ce4b772941842e2ea771318da430b4b9c2e3169
nystromb <bnystrom@siu.edu>
2016-01-13 11:29:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ttt-web-service
---
+++ spec/ttt_web_service/core_spec.clj
@@ -0,0 +1,7 @@
+(ns ttt-web-service.core-spec
+  (:require [speclj.core :refer :all]
+            [ttt-web-service.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
