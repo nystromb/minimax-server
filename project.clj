431ee9fa0094638c1c1b5d33b4ef93a481be45ea
Spencer <scarvill91@gmail.com>
2016-01-13 14:37:57 -0600

    Add java-httpserver jar to project resource-paths

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :resource-paths ["java-httpserver/target/httpserver-1.0-SNAPSHOT.jar"]
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
   :plugins [[speclj "3.3.1"]]
