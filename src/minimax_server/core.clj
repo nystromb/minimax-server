419799ea9b9a23cf5f12f7fee126d3bf546c9f9c
nystromb <bnystrom@siu.edu>
2016-01-18 11:39:57 -0600

    Added server main starting point

--- src/minimax_server/core.clj
+++ src/minimax_server/core.clj
@@ -1,1 +1,10 @@
-(ns minimax-server.core)
+ (ns minimax-server.core
+  (:import [scarvill.httpserver.server Server])
+  (:require [minimax-server.computer-player :as computer]
+            [minimax-server.server-configuration :refer [minimax-server-config]]))
+
+(def ttt-server
+  (new Server (minimax-server-config 5000 computer/stupid-move)))
+
+(defn main []
+  (.start ttt-server))
