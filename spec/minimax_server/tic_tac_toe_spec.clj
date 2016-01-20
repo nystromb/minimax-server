3a37b425d8358866a214da2d0a7d6994df848c43
nystromb <bnystrom@siu.edu>
2016-01-20 12:43:11 -0600

    Implemented player win detection

--- spec/minimax_server/tic_tac_toe_spec.clj
+++ spec/minimax_server/tic_tac_toe_spec.clj
@@ -3,7 +3,23 @@
             [minimax-server.tic-tac-toe :refer :all]))
 
 (describe "tic tac toe minimax helper functions"
+
+  (context "player win detection"
+
+    (it "returns true if player of given mark has won"
+      (let [board ["x" "x" "x"
+                   "_" "_" "_"
+                   "_" "_" "_"]]
+        (should= true (player-won? "x" board))))
+    
+    (it "returns false if player of given mark has not won" 
+      (let [board ["x" "o" "x"
+                   "x" "o" "o"
+                   "o" "x" "x"]]
+        (should= false (player-won? "o" board)))))
+    
   (context "game over detection"
+
     (it "should be false if game is not over"
       (let [board ["_" "_" "_"
                    "_" "_" "_"
@@ -16,14 +32,20 @@
                    "o" "x" "x"]]
         (should-be game-over? board)))
 
-    (it "should be true if a player has won at 0, 1, 2"
+    (it "should be true if a player x has a winning line"
       (let [board ["x" "x" "x"
                    "_" "_" "_"
                    "_" "_" "_"]]
         (should-be game-over? board)))
 
-    (it "should be true if a player has won at 3, 4, 5"
-      (let [board ["_" "_" "_"
-                   "x" "x" "x"
-                   "_" "_" "_"]]
-         (should-be game-over? board)))))
+    (it "should be true if player o has a winning line"
+      (let [board ["o" "_" "_"
+                   "_" "o" "_"
+                   "_" "_" "o"]]
+          (should-be game-over? board)))
+    
+    (it "should not be game over if there is no winning line"
+      (let [board ["o" "x" "o"
+                   "_" "x" "_"
+                   "o" "_" "_"]]
+          (should-not-be game-over? board)))))
--- src/minimax_server/tic_tac_toe.clj
+++ src/minimax_server/tic_tac_toe.clj
@@ -1,23 +1,17 @@
 (ns minimax-server.tic-tac-toe)
 
-(def win-lines '((0 1 2) (3 4 5) (6 7 8) (0 3 6) (1 4 7) (2 5 8) (1 4 8) (2 4 6)))
+(def board-line-indexes [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
 
-(defn check-win [mark board]
-  (loop [index 0
-         curr (first win-lines)]
-    (if (= mark (and (nth board (first curr)) 
-                     (nth board (second curr)) 
-                     (nth board (last curr)))) 
-                        true 
-                        (recur (inc index) (nth win-lines index)))))
+(def any? (comp boolean some))
+
+(defn winning-line? [line-indexes mark board]
+  (every? #(= mark %) (map #(nth board %) line-indexes))) 
+
+(defn player-won? [player-mark board]
+  (any? #(winning-line? % player-mark board) board-line-indexes)) 
 
 (defn is-draw? [board]
-  (nil? (some #(= "_" %) board)))
-
-(defn game-won? [mark board]
-  (check-win mark board))
+  (not-any? #(= "_" %) board))
 
 (defn game-over? [board]
-  (or (is-draw? board) (game-won? "x" board)))
-      
-
+  (or (is-draw? board) (player-won? "x" board) (player-won? "o" board)))
